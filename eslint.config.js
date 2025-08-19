e630e0bfeedc9426b00216f445cf7ad5e6804fed
Nick Dodd <endeedev@gmail.com>
2025-08-20 09:28:34 +1000

    feat: initial scaffolds

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const eslint = require('@eslint/js');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const globals = require('globals');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    },
+    {
+        files: ['**/*.js'],
+        rules: {
+            '@typescript-eslint/no-require-imports': 'off',
+        },
+    },
+);
